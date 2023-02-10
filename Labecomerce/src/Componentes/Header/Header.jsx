fcc417d397cc9dc140f610bc1791ee1910d7d17c
Rottschaefer <rottschaefer54@gmail.com>
2023-02-10 11:29:58 -0300

    feito

--- Labecomerce/src/Componentes/Header/Header.jsx
+++ Labecomerce/src/Componentes/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react"
-import { StyledHeader, StyledCart } from "./styleHeader"
+import { StyledHeader, StyledCart, StyledLogo, StyledContador, StyledNumber } from "./styleHeader"
 import carrinho from "../../assets/-shopping-cart_90604.png"
+import logo from "../../assets/Logo.png"
 
-export const Header = ({showCarrinhoHandler}) => {
+export const Header = ({showCarrinhoHandler, contador}) => {
 
 return(
     <StyledHeader>
-        <h2>INÍCIO</h2>
+        <StyledLogo src={logo}/>
+        {contador>0 && (<StyledContador>
+            <StyledNumber>{contador}</StyledNumber>
+        </StyledContador>)}
         <StyledCart onClick = {showCarrinhoHandler} src={carrinho}/> 
     </StyledHeader>
 )
